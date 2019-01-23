3b49d38c5b1b6dc912ac73dac7f00d3b537b0895
Artur Kudla <arturkapes@gmail.com>
2019-01-23 22:16:23 +0300

    docs: add routes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
-import React, { Component } from 'react';
-import './app.scss';
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import Home from './components/Home/Home';
+import Artists from './components/Artists/Artists';
+import MainLayout from './components/hoc/MainLayout';
+import PageNotFound from './components/PageNotFound/PageNotFound';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        App
-      </div>
-    );
-  }
+const App = () => {
+  return (
+    <MainLayout>
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/artists" component={Artists} />
+        <Route component={PageNotFound} />
+      </Switch>
+    </MainLayout>
+  )
 }
 
 export default App;
