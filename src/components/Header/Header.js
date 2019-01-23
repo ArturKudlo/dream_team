36bc86a29a7a9db3927b1930eff18bc6f778c360
Artur Kudla <arturkapes@gmail.com>
2019-01-23 22:31:24 +0300

    docs: Header component

---
+++ src/components/Header/Header.js
@@ -0,0 +1,30 @@
+/* eslint-disable no-unused-expressions */
+import React from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import { Link, withRouter } from 'react-router-dom';
+
+import './header.scss';
+
+const Header = props => {
+  return (
+    <div className="header-wrapper">
+      <AppBar position="static">
+        <Toolbar variant="dense">
+          <Typography variant="h6" color="inherit">
+            <Link to={'/'} className="header-link">
+              Home
+            </Link>
+            <Link to={'/artists'} className="header-link">
+              Artists
+            </Link>
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+}
+
+
+export default withRouter(Header);
