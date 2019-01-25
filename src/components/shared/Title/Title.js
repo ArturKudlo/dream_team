5f754717686d55199df160b1340ee07230c4d0cf
Artur Kudla <arturkapes@gmail.com>
2019-01-25 22:51:55 +0300

    feature: add Title component

---
+++ src/components/shared/Title/Title.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './title.scss';
+
+const Title = ({ text }) => {
+    return (
+        <div className="title-component">
+            {text}
+        </div>
+     );
+}
+ 
+export default Title;
